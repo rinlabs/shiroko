66981799e1b68488bffbdc6406e58dc949b3a8e4
rin <neolithium.redux@gmail.com>
2023-01-03 22:45:57 +0700

    Fixed branding

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,7 +8,7 @@
         <meta name="api_token" content="{{ Auth::user()->api_token }}">
     @endif
 
-    <title>@yield('title') - @if (config()->has('app.name')){{ config('app.name') }} @else My idlers @endif</title>
+    <title>@yield('title') - @if (config()->has('app.name')){{ config('app.name') }} @else shiroko @endif</title>
 
     @if(Session::get('dark_mode'))
         <link rel="stylesheet" href="{{ asset('css/bootstrap-dark.min.css') }}">
